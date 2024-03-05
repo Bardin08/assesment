9abb2dccb754295feb1245f3eef495ab985466bd
Bardin08 <vbardin810@gmail.com>
2024-03-06 00:01:22 +0200

    Add comment

--- Pixel.Shared/Infrastructure/RedisPublisher.cs
+++ Pixel.Shared/Infrastructure/RedisPublisher.cs
@@ -6,6 +6,7 @@ public class RedisPublisher(IConnectionMultiplexer redisConnection)
 {
     public async Task PublishAsync(string channel, string message)
     {
+        // no error handling, because if something wrong with messages publish I'd like this fail-fast
         var subscriber = redisConnection.GetSubscriber();
         await subscriber.PublishAsync(channel, message);
     }
