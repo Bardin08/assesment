cb3c6ccbe4ed0a6df47122589c165ceb39d690cb
Bardin08 <vbardin810@gmail.com>
2024-03-05 17:31:09 +0200

    Add load test to see pixel RPS

---
+++ Pixel.LoadTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using NBomber.CSharp;
+
+var scenario = Scenario.Create("pixel_request_scenario", async _ =>
+    {
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "load-tests-agent");
+        httpClient.DefaultRequestHeaders.Add("Referer",
+            "https://nbomber.com/docs/getting-started/hello_world_tutorial/");
+        var response = await httpClient.GetAsync("http://localhost:5123/track");
+
+        return response.IsSuccessStatusCode ? Response.Ok() : Response.Fail();
+    })
+    .WithoutWarmUp()
+    .WithLoadSimulations(
+        Simulation.RampingConstant(1000, during: TimeSpan.FromSeconds(60))
+    );
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
---
+++ Pixel.LoadTests/readme.md
@@ -0,0 +1,13 @@
+﻿## Scenario Stats
+
+Test results for single pixel and single storage instance
+
+load simulations:
+- ramping_constant, copies: 1000, during: 00:01:00
+
+| step               | stats                                                              |
+|--------------------|--------------------------------------------------------------------|
+| name               | global information                                                 |
+| `request count`    | all = `142493`, ok = `142493`, RPS = `2374.9`                      |
+| latency            | min = 1.76 ms, mean = 208.04 ms, max = 2240.3 ms, StdDev = 281.34  |
+| latency percentile | p50 = 89.34 ms, p75 = 161.79 ms, p95 = 822.78 ms, p99 = 1186.82 ms |
